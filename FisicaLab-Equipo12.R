a218489e3bb941304ca47f321a0cf019ca3eec72
Sergio <sdosoriogonzalez19@outlook.es>
2023-10-08 14:15:00 -0500

    Get Iris, check some values and generate some plots

--- FisicaLab-Equipo12.R
+++ FisicaLab-Equipo12.R
@@ -1,1 +1,11 @@
 summary(iris)
+mean(iris$Sepal.Length)
+mean(iris$Sepal.Width)
+mean(iris$Petal.Width)
+sd(iris$Sepal.Length)
+sd(iris$Sepal.Width)
+plot(iris$Sepal.Length)
+hist(iris$Sepal.Width, main="Mi histograma", xlab="Temperaturas",
+     ylab="Intensidades")
+boxplot(iris$Sepal.Length, main="Mi histograma", xlab="Temperaturas"
+        ,ylab="Intensidades")
